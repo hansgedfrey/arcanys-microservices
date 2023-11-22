33c2076b4c97a1d1aebfcf45377f3243b987b6bd
hansgedfrey <h.maligro@arcanys.com>
2023-11-22 12:49:45 +0800

    add gateway project

---
+++ ApiGateway/Gateway/ARC.ApiGateway/Program.cs
@@ -0,0 +1,20 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+await app.UseOcelot();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
